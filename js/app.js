f2c91f4cc72db439e18befe51d5a1db3b8e3964e
Tobias Lindgaard <tobias.private@gmail.com>
2012-04-18 22:37:05 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('<center><h1>Gambit Web-Magasin</h1></center>').prependTo("body");
+    $("#sondex2012").click(function () {
+	$("#article").load("articles/sondex2012.html");
+    });
+    $("#exam").click(function () {
+	$("#article").html
+('<a href="http://www.chessvideos.tv/chess-game-replayer.php?id=63365">Example Game</a>');
+    });
+    $("#index").click(function () {
+	$("#article").html('<p>Partier &oslash;nskes, gerne fra intern turnering</p>');
+	$('<p>Send til <a href="mailto:tobias.private@gmail.com">MinMail</a></p>').appendTo("#article");
+    });
+    $("#index").click();
+});
+
+
+
